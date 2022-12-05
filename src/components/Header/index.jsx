6b4ec759395590d4bbcc09f1da0855a9757a48d5
Braian Viacava de Avila <bbraian@Braians-MacBook-Pro.local>
2022-12-05 16:25:41 -0300

    updating

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -63,6 +63,7 @@ export function Header() {
                         <LangOption>
                             Portuguese
                         </LangOption>
+                        
                     </SelectLanguage>
                 </ConfigBox>
             </HeaderDesktop>
