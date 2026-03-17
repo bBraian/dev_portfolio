6038a1c331d49fd539fb8ade92475c8cbd510923
Braian <braianvoficial@gmail.com>
2026-03-17 00:47:31 -0300

    feat: clarity

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import './styles/globals.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import "./styles/globals.css";
+import Clarity from "@microsoft/clarity";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const clarityId = import.meta.env.CLARITY_ID;
+if (clarityId) {
+  Clarity.init(clarityId);
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
